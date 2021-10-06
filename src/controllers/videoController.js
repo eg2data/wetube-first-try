5258f6dceef1679b71708e4fb8f47a1e984a77cc
eg2data <eg2data@gmail.com>
2021-10-06 17:14:03 +0900

    10-06-first

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,10 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {
-    return res.render("home", { pageTitle: "Home", videos });
-  });
+// Video.find({}, (error, videos) => {});
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -14,16 +15,27 @@ export const getEdit = (req, res) => {
   return res.render("edit", { pageTitle:`Editing` });
 };
 export const postEdit = (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
-  return res.redirect(`/videos/${id}`);
+  const { title, description, hashtags } = req.body;
+  return res.redirect("/");
 };
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  const video = new Video({ 
+    title,
+    description,
+    createAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0, 
+      rating: 0,
+    }
+  });
+  const dbVideo = await video.save();
+  console.log(dbVideo); 
   return res.redirect("/");
 };
 
